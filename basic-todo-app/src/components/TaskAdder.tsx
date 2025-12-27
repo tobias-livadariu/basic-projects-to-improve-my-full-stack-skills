50a5445dadb85630fb6e733144c0518b7d3bcc35
Tobias Livadariu <tobias.livadariu@gmail.com>
2025-12-27 02:05:34 -0500

    some stuff

--- basic-todo-app/src/components/TaskAdder.tsx
+++ basic-todo-app/src/components/TaskAdder.tsx
@@ -1,7 +1,16 @@
 export default function TaskAdder() {
   return (
-    <div className="subblock">
-      TODO finish TaskAdder
+    <div className="subblock task-adder">
+      <h3 className="task-adder-header">Would you like to add a new task?</h3>
+      <div className="task-add-form">
+        <label>
+          <span className="task-add-label">Task name:</span>
+          <input type="text" />
+        </label>
+        <button className="task-add-button">
+          Create new task!
+        </button>
+      </div>
     </div>
   )
 }
