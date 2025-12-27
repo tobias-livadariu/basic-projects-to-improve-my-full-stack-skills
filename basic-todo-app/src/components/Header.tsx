ac2c8b20ee6c0c07b036e07b5189ae34c9c22781
Tobias Livadariu <tobias.livadariu@gmail.com>
2025-12-27 01:20:06 -0500

    Set up basic start

---
+++ basic-todo-app/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="main-header">
+      <h1>Tobias' Terrific Task Tracker</h1>
+      <h3>For all your task tracking needs</h3>
+    </div>
+  )
+}
